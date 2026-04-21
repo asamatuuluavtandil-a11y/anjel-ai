7de508ebf32208c314fe1c72057ff71509db5120
asamatuuluavtandil-a11y <asamatuuluavtandil@gmail.com>
2026-04-21 13:13:50 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import json
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler
